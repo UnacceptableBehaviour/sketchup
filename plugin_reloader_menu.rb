78ad52ebd492190dc20e14d19cb0e913634e9c4f
Simon <simon@ferdiesfoodlab.co.uk>
2019-07-01 21:30:00 +0100

    =plugin reloader - no menu/general polution

--- plugin_reloader_menu.rb
+++ plugin_reloader_menu.rb
@@ -1,6 +1,7 @@
 # First we pull in the standard API hooks.
 require 'sketchup.rb'
 require 'pp'
+require 'date'
 # Show the Ruby Console at startup so we can
 # see any programming errors we may make.
 SKETCHUP_CONSOLE.show
@@ -13,20 +14,46 @@ SKETCHUP_CONSOLE.show
 # from console
 # load 'plugin_reloader_menu.rb'
 
-# from submenu
-plugins_menu = UI.menu("Plugins")
+# from Extensions / Plugins menu
+# Extensions > Plugin - RELOADER > Reload 'Plugin Reloader'
 
-# from menu / code - define menu item in a loader plugin
-plugins_menu.add_item("Reload Plugin..") { puts "Plugin reloader - loaded" }
 
-submenu = plugins_menu.add_submenu("SUBMENU")
+module SKPluginDev
+  module PluginReloader
+    # Module Variables:
+    @@loaded ||= false
+    
+    # Run Once block:
+    # Add a menu item for the extension reloader alow development w/o restarting sketchup.
+    if not @@loaded
+      plugins_menu = UI.menu("Plugins")
+      
+      submenu = plugins_menu.add_submenu("Plugin - RELOADER")
+      
+      # add development plugins here
+      submenu.add_item("Reload 'Plugin Reloader'") { load 'plugin_reloader_menu.rb' }
+      
+      submenu.add_item("Reload 'Bevel Gear Maker'") { load 'bevel_gear_plugin.rb' }
+      
+      submenu.add_item("Reload 'Hello World'") { load 'plug_hello_world.rb' }
+      
+      puts "\n==> NAME: Plugin Reloader - Loaded - #{Time.now.strftime("%Y-%d-%m %H:%M:%S %Z")}"
+      
+      pp plugins_menu      
+            
+      @@loaded = true
+    end
+    # alternatively
+    #unless file_loaded?(__FILE__)
+    #  menu.add_item('Bevel Gear') {        
+    #    self.create_bevel_gear
+    #  }
+    #
+    #  file_loaded(__FILE__)
+    #end
 
-submenu.add_item("Plugin Reloader") { load 'plugin_reloader_menu.rb' }
+  end # module SKPluginDev
+end # module PluginReloader
 
-submenu.add_item("Bevel Gear Maker") { load 'bevel_gear_plugin.rb' }
-
-submenu.add_item("Hello World") { load 'plug_hello_world.rb' }
-
-puts "NAME: Plugin Reloader - Loaded"
-
-pp plugins_menu
+# visible from console on every re-load
+puts "\n==> NAME: Plugin Reloader - Loaded - #{Time.now.strftime("%Y-%d-%m %H:%M:%S %Z")}"
